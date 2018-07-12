24a944e93d1b85f6c63f518ce726b44ef730f880
Haryanto <quang.haryanto@gmail.com>
2018-07-12 16:13:29 +0700

    add 3 outlet

--- GitHubDemo/ViewController.swift
+++ GitHubDemo/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var topButton: UIButton!
+    
+    @IBOutlet weak var bottomLabel: UILabel!
     
     var temporary:String = ""
     
